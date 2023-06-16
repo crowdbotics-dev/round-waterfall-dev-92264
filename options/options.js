5ec16cbac2df94aafb7d052bfa8cef08aed86b22
Crowdbotics <engineering@crowdbotics.com>
2023-06-16 23:09:49 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "round-waterfall-dev-92264",
+  url: "https://round-waterfall-dev-92264.botics.co",
+  api: "https://round-waterfall-dev-92264.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
